59bfefab57450783639144e5aaecad33dce0605b
cleiton <cleiton_amigos2013@hotmail.com>
2018-03-28 13:46:03 -0300

    Aula javascript

---
+++ operadores/js/script.js
@@ -0,0 +1,16 @@
+
+
+var x =  6;
+
+ alert( x  "7");
+
+
+
+ // == igual 
+ // === identidade (valor e tipo de dado)
+ // != diferente
+ //!== identidade (valor tipo de dados são diferentes)
+ // > maior
+ // < menor
+ // >= maior ou igual
+ // <= menor ou igual
